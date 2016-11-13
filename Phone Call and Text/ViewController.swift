f7cb0b8ad62168356de6c1e96c46fd393a0dca70
kris-randen <kris.randen@gmail.com>
2016-11-12 20:43:30 -0500

    Works perfectly. Both iPhone call and Text.

--- Phone Call and Text/ViewController.swift
+++ Phone Call and Text/ViewController.swift
@@ -7,9 +7,49 @@
 //
 
 import UIKit
+import MessageUI
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MFMessageComposeViewControllerDelegate {
 
+    
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    
+    @IBAction func call(_ sender: Any)
+    {
+        callNumber(phoneNumber: phoneNumberTextField.text!)
+    }
+    
+    @IBAction func text(_ sender: Any)
+    {
+        sendText(phoneNumber: phoneNumberTextField.text!)
+    }
+    
+    func callNumber(phoneNumber:String)
+    {
+        if let phoneCallURL:NSURL = NSURL(string: "tel://\(phoneNumber)") {
+            let application:UIApplication = UIApplication.shared
+            if (application.canOpenURL(phoneCallURL as URL)) {
+                application.openURL(phoneCallURL as URL);
+            }
+        }
+    }
+    
+    func sendText(phoneNumber: String)
+    {
+        if (MFMessageComposeViewController.canSendText()) {
+            let controller = MFMessageComposeViewController()
+            controller.body = ""
+            controller.recipients = [phoneNumber]
+            controller.messageComposeDelegate = self
+            self.present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        self.dismiss(animated: true, completion: nil)
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
